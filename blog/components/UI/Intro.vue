e587b753ea7a0f02b83d058eab82a57582eb2884
Mark <marktim39@gmail.com>
2020-11-17 22:10:20 +0600

    upd

---
+++ blog/components/UI/Intro.vue
@@ -0,0 +1,32 @@
+<template>
+    <section class="intro">
+        <div class="container">
+            <h2> {{ title }} </h2>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    props: {
+        title: {
+            type: String,
+            required: true
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+.intro {
+    width: 100%;
+    margin: 30px 0;
+    // padding: 30px 0;
+    text-align: center;
+    color: white;
+    background-color: #4b40e3;
+    h1 {
+        font-size: 26px;
+    }
+}
+</style>
---
+++ blog/components/system/Header.vue
@@ -0,0 +1,44 @@
+<template>
+<header>
+                <div class="navbar">
+                    <div class="container">
+                        <div class="navbar-content">
+                            <div class="logo">Shimada clan blog</div>
+                                
+                                <ul class="navbar-list">
+                                    
+                                    <li class="navbar-item" v-for="link in links" :key="link.title">
+                                        <nuxt-link class="navbar-link" :title="link.title" :to="link.url"> {{ link.title}} </nuxt-link>
+                                    </li>
+
+                                </ul>   
+                        </div> 
+                    </div>
+                </div>
+            </header>
+    
+
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            links: [
+                { title: 'Home', url: '/'},
+                { title: 'About', url: '/about'},
+                { title: 'Posts', url: '/posts'}
+            ]
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+.navbar-link {
+    &.nuxt-link-exact-active {
+            color: #4b40e3;
+            // color: #b00b69;
+        }
+    }
+</style>
